a8acee36a2600b5b694ba641c5e8cfdef87cdde4
Marc Farra <marcfarra@gmail.com>
2017-06-09 15:53:17 -0400

    Add deploy scripts

---
+++ .build_scripts/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${STAGING_BRANCH} ]; then
+	echo "Building site"
+	gulp stage
+elif [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${PRODUCTION_BRANCH} ]; then
+	echo "Building site"
+	gulp prod
+else
+	echo "Not building"
+fi
