f507cc5dfdbc6b155660ec6634f8bd685f4f8326
maxgrossman <m.grossman1813@gmail.com>
2017-07-12 08:28:53 -0400

    see memory usage throughout build

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -e # halt script on error
-
+free -m
 # If this is the deploy branch, push it up to gh-pages
 echo "Get ready, we're pushing to gh-pages!"
 cd _site
+free -m
 rm -rf .git
-
+free -m
 SSH_REPO="git@github.com:${TRAVIS_REPO_SLUG}.git"
 SHA=`git rev-parse --verify HEAD`
 
 # init the repo
 git init
+free -m
 git config user.name "Travis-CI"
 git config user.email "travis@somewhere.com"
 git add .
+free -m
 git commit -m "CI deploy to master ${SHA}"
 git show-ref
 # get deploy key
