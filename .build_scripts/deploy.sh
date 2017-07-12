b87eae0f42790e04252389ffb013442482f51417
maxgrossman <m.grossman1813@gmail.com>
2017-07-12 08:37:07 -0400

    redo free statement

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 set -e # halt script on error
-free -m
+free
 # If this is the deploy branch, push it up to gh-pages
 echo "Get ready, we're pushing to gh-pages!"
 cd _site
-free -m
+free
 rm -rf .git
-free -m
+free
 SSH_REPO="git@github.com:${TRAVIS_REPO_SLUG}.git"
 SHA=`git rev-parse --verify HEAD`
 
 # init the repo
 git init
-free -m
+free
 git config user.name "Travis-CI"
 git config user.email "travis@somewhere.com"
 git add .
-free -m
+free
 git commit -m "CI deploy to master ${SHA}"
 git show-ref
 # get deploy key
