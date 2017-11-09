968eb3be662f657747e55d0f0a2aed87c95b28e6
dale@normalhabit.com <dale@normalhabit.com>
2017-11-08 20:45:01 -0800

    realized we won't need a lot of this code becuase tm3 now returns aoi centroid

--- .build_scripts/generateTasks.js
+++ .build_scripts/generateTasks.js
@@ -2,14 +2,9 @@ var fs = require('fs');
 var path = require('path');
 var flow = require('flow');
 var request = require('request');
-var turf = require('turf');
 var moment = require("moment");
 
-
-var timestamp = moment().format('YYYYMMDD-HHmmss');
 var missingmaps = [];
-// var projectList = [];
-var tasksFc = { type: 'FeatureCollection', features: [] };
 
 var throttleProjects = function(cb){
   var targetCount = 5000;
@@ -30,25 +25,17 @@ var throttleProjects = function(cb){
 var fetchProjectData = function(projectNumber, cb) {
   request({
     method: 'GET',
-    uri: "http://tasks.hotosm.org/api/v1/project/" + projectNumber + "/summary"
+    uri: "https://tasks.hotosm.org/api/v1/project/" + projectNumber + "/summary"
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var jsonResponse = JSON.parse(body);
+
       if(jsonResponse){
         /// capitalization or presence/lack of a space in Missing Maps shouldn't matter
         var nameCheck = jsonResponse.name.replace(/\s+/g, '').toLowerCase().indexOf("missingmaps");
         if(nameCheck !== -1){
-          // projectList.push(projectNumber); // # # # compile list of project numbers to next fetch detailed task data
-          var projectObj = {
-            "task_number": projectNumber,
-            "created" : jsonResponse["created"].slice(0,10),
-            "name": jsonResponse["name"].replace(/"/g,""),
-            "author": jsonResponse["organisationTag"],
-            "status":jsonResponse["status"],
-            "done": jsonResponse["percentMapped"],
-            "validated": jsonResponse["percentValidated"]
-          }
-          missingmaps.push(projectObj);
+
+          missingmaps.push(jsonResponse);
           console.log("missingmaps :  " + projectNumber);
         } else { console.log("other task  :  " + projectNumber); }
       }
@@ -61,67 +48,13 @@ var fetchProjectData = function(projectNumber, cb) {
   });
 }
 
-var throttleTasks = function(cb){
-  var targetCount = 0;
-  var counter = 0;
-  targetCount = missingmaps.length;
-  for (var i=0;i<targetCount;i++) {
-     (function(ind) {
-         setTimeout(function(){
-           // # # # throttle process to limit the speed of calls to download files from the server
-           fetchTaskData(ind, function(){
-             counter ++;
-             if(counter === targetCount){ cb(); }
-           });
-         }, 500 + (100 * ind));
-     })(i);
-  }
-}
-
-var fetchTaskData = function(prjIndex, cb) {
-  var thisPrj = missingmaps[prjIndex];
-  console.log("http://tasks.hotosm.org/api/v1/project/" + thisPrj["task_number"] + ".json")
-  request({
-    method: 'GET',
-    uri: "http://tasks.hotosm.org/api/v1/project/" + thisPrj["task_number"] + ".json"
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var jsonResponse = JSON.parse(body);
-      for(var i=0; i<jsonResponse.features.length; i++){
-        var tile = jsonResponse.features[i];
-        var thisState = tile.properties.state;
-        // 2 is done and 3 is validated
-        // https://github.com/hotosm/osm-tasking-manager2/wiki/API#list-of-tasks-with-state-and-lock-status
-        var tileProp = {
-            "task": thisPrj["task_number"],
-            "created": thisPrj["created"],
-            "state": taskStatus
-        };
-        tasksFc.features.push(turf.feature(tile.geometry, tileProp));
-        }
-
-      }
-      console.log("processed tasks for #" + thisPrj["task_number"]);
-      cb();
-  });
-}
-
-var parseTasks = flow.define(
-  function(){
-    throttleTasks(this);
-  },
-  function(){
-    var filePath = path.join('./', "tasks.geojson");
-    fs.writeFile(filePath, JSON.stringify(tasksFc));
-  }
-);
-
 var parseProjects = flow.define(
   function(){
     throttleProjects(this);
   },
   function(){
-    parseTasks();
+    var filePath = path.join('./', "tasks.geojson");
+    fs.writeFile(filePath, JSON.stringify(missingmaps));
   }
 );
 
